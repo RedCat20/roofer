5fef786d3e34c54fe955745f9991f99279ab0eff
d.klymchyk <d.klymchyk@ccs.court.gov.ua>
2024-10-29 22:58:24 +0200

    add env

--- src/store/api/dictionary.ts
+++ src/store/api/dictionary.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const dictionariesApi = createApi({
 	reducerPath: 'dictionariesApi',
 	// baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:4000' }),
-	baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }),
+	baseQuery: fetchBaseQuery({ baseUrl: process.env.BASE_URL }),
 	endpoints: builder => ({
 		getDictionaries: builder.query<any, string>({
 			// query: name => `/dictionaries`,
