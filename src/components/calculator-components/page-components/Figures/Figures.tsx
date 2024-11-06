554c1e4757caad91f4e346c50f9a1c66e5013f76
d.klymchyk <d.klymchyk@ccs.court.gov.ua>
2024-11-06 11:47:22 +0200

    add custom figure

--- src/components/calculator-components/page-components/Figures/Figures.tsx
+++ src/components/calculator-components/page-components/Figures/Figures.tsx
@@ -63,7 +63,7 @@ export const Figures: FC<Props> = ({ onChooseFigure }) => {
 							alt='Прямокутник'
 						/>
 					</button>
-					<button
+					{/* <button
 						className={clsx(
 							styles.figureButton,
 							selectedFigure === FIGURES.Trapezoid ? styles.activeFigure : ''
@@ -96,7 +96,7 @@ export const Figures: FC<Props> = ({ onChooseFigure }) => {
 							src={triangleIcon}
 							alt='Трикутник'
 						/>
-					</button>
+					</button> */}
 					<button
 						className={clsx(
 							styles.figureButton,
